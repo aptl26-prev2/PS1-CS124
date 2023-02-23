9ec4d6905c388fd2f027fdbd6bbd3ab9765bea44
aptl26 <aghyad.deeb.2002@gmail.com>
2023-02-22 22:43:07 -0500

    Fixed runexperiments

--- runexperiments.py
+++ runexperiments.py
@@ -3,15 +3,15 @@
 import os
 
 os.system('make all')
-# for D in range(2, 5):
-#     for n in range(1, 19):
-#         N = 2**n 
-#         command = './randmst ' + str(N) + ' 5 ' + str(D)
-#         # print(command)
-#         os.system(command)
-
 for n in range(1, 19):
     N = 2**n 
     command = './randmst ' + str(N) + ' 5 ' + str(0)
     # print(command)
     os.system(command)
+    
+for D in range(2, 5):
+    for n in range(1, 19):
+        N = 2**n 
+        command = './randmst ' + str(N) + ' 5 ' + str(D)
+        # print(command)
+        os.system(command)
